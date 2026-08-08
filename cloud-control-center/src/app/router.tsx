71b777482e3124f2c7bf81e31fc9272a3107f28c
Chandan Padal <CZ0134@cztech.com>
2026-08-08 05:52:10 +0530

    bootstrap react

---
+++ cloud-control-center/src/app/router.tsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import MainLayout from "@/layouts/MainLayout";
+
+import Home from "@/pages/Home/Home";
+import NotFound from "@/pages/NotFound/NotFound";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <MainLayout />,
+        children: [
+            {
+                index: true,
+                element: <Home />,
+            },
+        ],
+    },
+
+    {
+        path: "*",
+        element: <NotFound />,
+    },
+]);
---
+++ cloud-control-center/src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+    return (
+        <section>
+            <h1>Cloud Control Center</h1>
+
+            <p>
+                Building resilient cloud platforms across
+                AWS, Azure and Google Cloud.
+            </p>
+        </section>
+    );
+}
